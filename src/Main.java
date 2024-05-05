3da7a3304d66f06894af9dca91a6f26882557fca
nabrain <michal2002.60@gmail.com>
2024-05-05 17:56:13 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import company.abstracts.Employee;
 import company.models.Manager;
 import company.models.Worker;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Worker workerZero = new Worker("Jame", 12000, 0);
-        Worker workerOne = new Worker("Jami", 1200, 1);
-        Worker workerTwo = new Worker("Jimmy", 4000, 2);
-        Worker workerThree = new Worker("Jil", 7000, 3);
+        Worker workerZero = new Worker("Jame", 12000, 0, "08-08-2008", "chef");
+        Worker workerOne = new Worker("Jami", 1200, 1, "12-12-2012", "cook");
+        Worker workerTwo = new Worker("Jimmy", 4000, 2, "11-11-2011", "cook");
+        Worker workerThree = new Worker("Jil", 7000, 3, "10-10-2010", "waiter");
 
-        Manager managerZero = new Manager("Manny", 1200000, 0);
+        Manager managerZero = new Manager("Manny", 1200000, 0, "09-09-2009", "chairman");
+
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        employees.add(workerZero);
+        employees.add(workerOne);
+        employees.add(workerTwo);
+        employees.add(workerThree);
+
+        employees.add(managerZero);
     }
 }
--- src/company/abstracts/Employee.java
+++ src/company/abstracts/Employee.java
@@ -7,11 +7,15 @@ public abstract class Employee implements Employable {
     private String name;
     private double salary;
     private int id;
+    private String hireDate;
+    private String position;
 
-    public Employee(String name, double salary, int id) {
+    public Employee(String name, double salary, int id, String hireDate, String position) {
         this.name = name;
         this.salary = salary;
         this.id = id;
+        this.hireDate = hireDate;
+        this.position = position;
     }
 
     public String getName() {
@@ -20,5 +24,17 @@ public abstract class Employee implements Employable {
 
     public double getSalary() {
         return salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public int hashCode() {
+        return id;
     }
 }
