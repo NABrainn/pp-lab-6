06c4da07864d627c21e21118c2779bf62d80ac9d
nabrain <michal2002.60@gmail.com>
2024-05-05 17:45:27 +0200

    zad1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import company.models.Manager;
+import company.models.Worker;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker workerZero = new Worker("Jame", 12000, 0);
+        Worker workerOne = new Worker("Jami", 1200, 1);
+        Worker workerTwo = new Worker("Jimmy", 4000, 2);
+        Worker workerThree = new Worker("Jil", 7000, 3);
+
+        Manager managerZero = new Manager("Manny", 1200000, 0);
+    }
+}
