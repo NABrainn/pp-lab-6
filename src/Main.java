b634107c76aec4cc88e3323c5947c913cb317cee
nabrain <michal2002.60@gmail.com>
2024-05-05 18:01:05 +0200

    zad1

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,19 @@ public class Main {
 
         Manager managerZero = new Manager("Manny", 1200000, 0);
 
+        System.out.println(workerZero.getSalary());
         workerZero.work();
+
+        System.out.println(workerOne.getSalary());
         workerOne.work();
+
+        System.out.println(workerTwo.getSalary());
         workerTwo.work();
+
+        System.out.println(workerThree.getSalary());
         workerThree.work();
 
+        System.out.println(managerZero.getSalary());
         managerZero.work();
     }
 }
